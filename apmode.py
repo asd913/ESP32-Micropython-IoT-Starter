e66bf32213c6e0ba9a719779cee201a7118b1f5a
asd913 <76713295+asd913@users.noreply.github.com>
2021-01-07 21:48:41 -0700

    Update apmode.py
    Changed some of the code so that the IoT spash html lists the currently available ssids as radio buttons. Also, new code uses POST instead of GET to pass the wifi credentials.

--- apmode.py
+++ apmode.py
@@ -1,7 +1,27 @@
+import network
+import socket
+ssid = 'Update-IoT-WIFI'
+password = '123456789'
+ap_authmode = 3
+sckt = None
+
+
 # ---------------------------------------------------------------------------------------------------- #
-# This is a simple form html so that we can get the ssid and the password
+# Send a header based on the payload length.
 # ---------------------------------------------------------------------------------------------------- #
-def web_page(ssid, pwd):
+def send_header(client, status_code=200, content_length=None ):
+    client.sendall("HTTP/1.0 {} OK\r\n".format(status_code))
+    client.sendall("Content-Type: text/html\r\n")
+    if content_length is not None:
+      client.sendall("Content-Length: {}\r\n".format(content_length))
+    client.sendall("\r\n")
+
+
+    
+# ---------------------------------------------------------------------------------------------------- #
+# This is a simple form html that lists the ssid and allows the user to type a password
+# ---------------------------------------------------------------------------------------------------- #
+def web_page(ssids):
      
     html_page = """<!DOCTYPE html>
         <html>
@@ -10,78 +30,191 @@ def web_page(ssid, pwd):
             <title>IoT WIFI Update</title>
         </head>
           <body>
-            <h2>Enter New WIFI Credentials</h2>
-            <form>
-              <label for="ssid">SSID: </label>
-              <input type="text" id="fname" name="ssid" value=""" + ssid + """><br><br>
-              <label for="password">Password: </label>
-              <input type="text" id="password" name="password" value=""" + pwd + """><br><br><br><br>
-              <input type="submit" value="Submit">
-            </form> 
-          </body>
+            <h2>IoT New WIFI Credentials</h2>
+            <form action="entered" method="post">
+                <table style="margin-left: auto; margin-right: auto;">
+                    <tbody>
+    """
+    for x in ssids:  #this loop will list all the ssids passed into the function
+        html_page = html_page + """\
+                        <tr>
+                            <td colspan="2">
+                                <input type="radio" name="ssid" value=""" + x + """ />""" + x + """
+                            </td>
+                        </tr>
+        """
+    html_page = html_page +  """\
+                        <tr>
+                            <td>Password:</td>
+                            <td><input name="password" type="password" /></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p style="text-align: center;">
+                    <input type="submit" value="Submit" />
+                </p>
+            </form>
         </html>"""  
     return html_page
 
 
+    
+# ---------------------------------------------------------------------------------------------------- #
+# Find the length of the data to send to the client and send them the data.
+# ---------------------------------------------------------------------------------------------------- #
+def send_response(client, payload, status_code=200):
+    content_length = len(payload)
+    if content_length > 0:
+        client.sendall(payload)
+
+
+    
+# ---------------------------------------------------------------------------------------------------- #
+# Returns the HTML for telling the user we are trying the new wifi credentials.
+# ---------------------------------------------------------------------------------------------------- #
+def newCredsPage():
+    html_page = """
+        <html>
+            <center>
+                <br><br>
+                <h1 style="color: #5e9ca0; text-align: center;">
+                    <span style="color: #ff0000;">
+                        ESP trying new credentials now.<br>
+                    </span>
+                </h1>
+                <br><br>
+            </center>
+        </html>"""  
+    return html_page
+
+
+    
+# ---------------------------------------------------------------------------------------------------- #
+# Returns the HTML for telling the user the new wifi credentials failed.
+# ---------------------------------------------------------------------------------------------------- #
+def FailedCredsPage():
+    html_page = """
+        <html>
+            <center>
+                <br><br>
+                <h1 style="color: #5e9ca0; text-align: center;">
+                    <span style="color: #AA0000;">
+                        New credentials failed, please reenter.<br>
+                    </span>
+                </h1>
+                <br><br>
+            </center>
+        </html>"""  
+    return html_page
+
+
+    
+# ---------------------------------------------------------------------------------------------------- #
+# Returns the HTML for telling the user the new wifi credentials succeeded.
+# ---------------------------------------------------------------------------------------------------- #
+def newCredsSuccessPage():
+    html_page = """
+        <html>
+            <center>
+                <br><br>
+                <h1 style="color: #5e9ca0; text-align: center;">
+                    <span style="color: #f0000bb;">
+                        SUCCESS!!<br>
+                    </span>
+                </h1>
+                <br><br>
+            </center>
+        </html>"""  
+    return html_page
+
+
+    
 # ---------------------------------------------------------------------------------------------------- #
 # Extract the ssid and password from the response text
 # ---------------------------------------------------------------------------------------------------- #
 def pullData(request1):
-    splitRequestStr = request1.split('?ssid=')
-    ssidpwd_combined = splitRequestStr[1].split(' HTTP/1.1')
-    ssidpwdsplit= ssidpwd_combined[0].split('&password=')
+    splitRequestStr = request1.split('ssid=')
+    ssidpwdsplit= splitRequestStr[1].split('&password=')
     ssid1 = ssidpwdsplit[0].rstrip("\n").rstrip()
-    password1 = ssidpwdsplit[1].rstrip("\n").rstrip()
+    password1 = ssidpwdsplit[1].rstrip("\n").rstrip("'").rstrip()
     return ssid1,password1
 
+def stop():
+    global sckt
+
+    if sckt:
+        sckt.close()
+        sckt = None
+
+def checkWifi(ssid1, password1,station):
+    import time
+    station.connect(ssid1, password1)
+    time.sleep(5)
+    
+    return station.isconnected()
+
+    
 
 # ---------------------------------------------------------------------------------------------------- #
 # This function will start an Access Point with hardcoded ssid. It will serve the webpage html and wait
 # for a response that includes a ssid filled in. This method doesn't seem very secure so improvements 
 # would be welcome. 
 # ---------------------------------------------------------------------------------------------------- #
-def setupAP(ssidOld,passwordOld):
+def setupAP(ssids, station):
 
-    import network
-    ssid = 'Update-IoT-WIFI'
-    password = '123456789'
 
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    stop()
+    
     ap = network.WLAN(network.AP_IF)
-    ap.active(True)
-    ap.config(essid=ssid, password=password)
-    while not ap.active():
-        pass
+    sckt = socket.socket()
+    if not ap.active():
+        ap.active(True)
+        ap.config(essid=ssid, password=password, authmode=ap_authmode)
+        while not ap.active():
+            pass
+        sckt.bind(addr) 
+        sckt.listen(5)     
     print('network config:', ap.ifconfig())
-
 
     # create the socket
     # AF_INET == ipv4
     # SOCK_STREAM == TCP
-    import socket
-    sckt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sckt.bind(('',80)) 
-    sckt.listen(5)     
+
 
     # Function for creating the
-    request1 = ""
-    while request1.find("ssid")==-1:  # wait for a response that includes ssid in it
+    request = b""
+    while 'ssid=' not in str(request):  # wait for a response that includes ssid in it
         conn, addr = sckt.accept() #clientsocket, address of client
         print("Got connection from %s" % str(addr))
-        response = web_page(ssidOld,passwordOld)#Prepare the webpage with old ssid and password stored
-                                                #so the user can see what it was set to previously.
-        conn.send('HTTP/1.1 200 OK\n')
-        conn.send('Content-Type: text/html\n')
-        conn.send('Connection: close\n\n')
+        response = web_page(ssids)
+        send_header(conn)
         conn.sendall(response) #Send the webpage.
-        request=conn.recv(1024)
-        request1 = str(request) # Convert the url returned to string
-        print("Content %s" % request1)
+        request = b""
+        try:
+            while '\r\n\r\n' not in request:
+                request += conn.recv(512)
+        except OSError:
+            pass
+        print("Content ", str(request))
+        if 'ssid=' in str(request):
+            send_response(conn, newCredsPage())
+            ssid1,password1 = pullData(str(request))
+            if not checkWifi(ssid1, password1,station):
+                print("Credentials, failed")
+                send_response(conn, FailedCredsPage())
+                request = b"" #force a new loop
+                station.disconnect()
+            else:
+                send_response(conn, newCredsSuccessPage())
+                station.disconnect()
         conn.close()
-    
+            
     # Turn off the AP
-    ap.disconnect()
+    sckt.close()
+    sckt = None
     ap.active(False)
     
     #extract the ssid and password from the response text
-    ssid1,password1 = pullData(request1)
+    
     return ssid1,password1
